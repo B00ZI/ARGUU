03e01d841a0cff41927e5c2442e37bf4bdd7dd5f
B00ZI <iraamaneyahya@gmail.com>
2026-01-14 23:02:49 +0100

    feat: add publisher_id to Book model fillable attributes; define fillable attributes in Publisher model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    protected $fillable = [ 'title', 'author', 'pages', 'published_year'];
+    protected $fillable = [ 'title', 'author', 'pages', 'published_year' , 'publisher_id'];
 }
 
