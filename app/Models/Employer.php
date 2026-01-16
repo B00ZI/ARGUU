e38634c259c7f66be75b8891fc445fb8ff1dd804
B00ZI <iraamaneyahya@gmail.com>
2026-01-16 21:32:12 +0100

    feat: add jobs relationship to Employer model and employer relationship to JobListing model

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\JobListing;
 
 class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    public function jobs(){
+          
+        return $this->hasMany(JobListing::class);
+
+    }
 }
--- app/Models/JobListing.php
+++ app/Models/JobListing.php
@@ -3,16 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 
 class JobListing extends Model
-{
+{   
     use HasFactory;
+
+
 
     protected $table = "job_listing";
     protected $primaryKey = 'job_id';
     protected $fillable = ['title', 'salary'];
+
+    public function employer(){
+          
+        return $this->belongsTo(Employer::class);
+
+    }
 }
