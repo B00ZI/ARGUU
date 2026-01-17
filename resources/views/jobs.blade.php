4e35ca4949c8de65978b6e8a3a83844a46263332
B00ZI <iraamaneyahya@gmail.com>
2026-01-17 20:28:23 +0100

    feat: add fruitcake/laravel-debugbar to development dependencies; update composer.lock; enhance jobs view layout

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,14 +2,16 @@
    <x-slot name="title">
       jobs list
    </x-slot>
-   <ul>
+   <div class="flex flex-wrap p-5 gap-y-5 gap-x-3 justify-center  ">
       @foreach ( $jobs as $job)
       
-      <a href="/jobs/{{ $job['id'] }}">
-      <li> <b>{{ $job['title'] }}</b> pays: {{ $job['salary'] }} per year </li>
+      <a class= "block border grow border-gray-500 px-4 py-2" href="/jobs/{{ $job['id'] }}">
+        <span class="block text-blue-950  text-xs mb-2 font-bold"> {{ $job->employer->employer }}</span>
+        
+       <b>{{ $job['title'] }}</b> pays: {{ $job['salary'] }} per year 
       </a>
       @endforeach
-   </ul>
+   </div>
 
 
 
