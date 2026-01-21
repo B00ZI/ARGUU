5c5138887c4ea7ffa53acbd4330b947a7ea378cd
B00ZI <iraamaneyahya@gmail.com>
2026-01-21 21:21:01 +0100

    added a foenkey in employer for user and added the relation in employer model

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,15 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\JobListing;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
 
-    public function jobs(){
-          
-        return $this->hasMany(JobListing::class);
+    public function jobs()
+    {
 
+        return $this->hasMany(JobListing::class);
+    }
+
+    public function user(): BelongsTo
+    {
+
+        return $this->belongsTo(JobListing::class);
     }
 }
