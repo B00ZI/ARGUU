4cf6912c867aa07b8892138bf271d5c940bb0441
B00ZI <iraamaneyahya@gmail.com>
2026-01-23 23:25:48 +0100

    make the jobposted blade  use job

--- resources/views/mail/job-posted.blade.php
+++ resources/views/mail/job-posted.blade.php
@@ -1,10 +1,12 @@
 <x-mail::message>
 # Introduction
 
+{{ $job->title }}
+
 The body of your message.
 
-<x-mail::button :url="''">
-Button Text
+<x-mail::button :url="url('/jobs/' . $job->id)">
+View Job
 </x-mail::button>
 
 Thanks,<br>
